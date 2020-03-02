4ab3905f42bab988cf6ac165e36d3812b1e73958
vldmr11080 z <vldmr11080@gmail.com>
2020-03-02 23:13:20 +0100

    Add icons and improve custom layout editor window

---
+++ src/modules/fancyzones/editor/FancyZonesEditor/Converters/NewModelToVisibilityConverter.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using FancyZonesEditor.Models;
+
+namespace FancyZonesEditor.Converters
+{
+    public class NewModelToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (((LayoutModel)value).Type != LayoutType.Blank) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
